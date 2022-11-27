e82d72993a6f3f4aafc5f384de8516cad9203a32
bloodlustyt <67737806+bloodlustyt@users.noreply.github.com>
2022-11-27 21:27:47 +0530

    Update frontend.py

--- main/plugins/frontend.py
+++ main/plugins/frontend.py
@@ -20,6 +20,29 @@ message = "Send me the message link you want to start saving from, as a reply to
 # Make these codes shorter and clean
 # ofc will never do it. 
 
+process1 = []
+timer = []
+
+#Set timer to avoid spam
+async def set_timer(event, list1, list2):
+    now = time.time()
+    list2.append(f'{now}')
+    list1.append(f'{event.sender_id}')
+    await event.client.send_message(event.chat_id, 'You can start a new process again after 1 minute.')
+    await asyncio.sleep(60)
+    list2.pop(int(timer.index(f'{now}')))
+    list1.pop(int(process1.index(f'{event.sender_id}')))
+    
+#check time left in timer
+async def check_timer(event, list1, list2):
+    if f'{event.sender_id}' in list1:
+        index = list1.index(f'{event.sender_id}')
+        last = list2[int(index)]
+        present = time.time()
+        return False, f"You have to wait {60-round(present-float(last))} seconds more to start a new process!"
+    else:
+        return True, None
+
 @Drone.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def clone(event):
     if event.is_reply:
@@ -37,10 +60,15 @@ async def clone(event):
         await event.reply(r)
         return
     edit = await event.reply("Processing!")
+    s, t = await check_timer(event, process1, timer) 
+    if s == False:
+        return await edit.edit(t)
     if 't.me/+' in link:
         q = await join(userbot, link)
         await edit.edit(q)
+        await set_timer(event, process1, timer) 
         return 
     if 't.me/' in link:
         await get_msg(userbot, Bot, event.sender_id, edit.id, link, 0)
+        await set_timer(event, process1, timer) 
         
